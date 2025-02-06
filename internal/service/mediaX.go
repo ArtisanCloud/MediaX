3e055823e231567924514d110367aa3b68c7ee1c
Matrix-X <matrix-x@artisan-cloud.com>
2025-02-06 23:22:27 +0800

    feat(plugin): remove plugin manager

---
+++ internal/service/mediaX.go
@@ -0,0 +1,13 @@
+package service
+
+import (
+	"github.com/ArtisanCloud/MediaXCore/pkg/logger"
+)
+
+type MediaXService struct {
+	Logger *logger.Logger
+}
+
+func NewMediaXService() *MediaXService {
+	return nil
+}
