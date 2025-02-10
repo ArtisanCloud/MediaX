8cd5ccea9107c8064e6a4fb9e40d0fe7886be97a
Matrix-X <matrix-x@artisan-cloud.com>
2025-02-10 23:47:15 +0800

    feat(config): same tag for json and yaml

--- pkg/client/config/config.go
+++ pkg/client/config/config.go
@@ -7,16 +7,16 @@ type MediaXConfig struct {
 }
 
 type LocalConfig struct {
-	*WeChatOfficialAccountConfig `yaml:"wechat_config" json:"weChatConfig"`
-	*DouYinConfig                `yaml:"douyin_config" json:"douYinConfig"`
-	*RedBookConfig               `yaml:"redbook_config" json:"redBookConfig"`
+	*WeChatOfficialAccountConfig `yaml:"wechat_config" json:"wechat_config"`
+	*DouYinConfig                `yaml:"douyin_config" json:"douyin_config"`
+	*RedBookConfig               `yaml:"redbook_config" json:"redbook_config"`
 }
 
 type AppConfig struct {
-	BaseUri   string  `yaml:"base_uri" json:"baseUri"`
-	ProxyUri  string  `yaml:"proxy_uri" json:"proxyUri"`
+	BaseUri   string  `yaml:"base_uri" json:"base_uri"`
+	ProxyUri  string  `yaml:"proxy_uri" json:"proxy_uri"`
 	Timeout   float64 `yaml:"timeout" json:"timeout"`
-	AppID     string  `yaml:"app_id" json:"appId"`
-	AppSecret string  `yaml:"app_secret" json:"appSecret"`
-	HttpDebug bool    `yaml:"http_debug" json:"httpDebug"`
+	AppID     string  `yaml:"app_id" json:"app_id"`
+	AppSecret string  `yaml:"app_secret" json:"app_secret"`
+	HttpDebug bool    `yaml:"http_debug" json:"http_debug"`
 }
